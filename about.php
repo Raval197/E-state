3937e5d47b80f574a2c66cec8895a513e4c48d5c
Raval balkrishna <112169855+Raval197@users.noreply.github.com>
2024-02-21 18:09:54 +0530

    Update about.php
    g

--- about.php
+++ about.php
@@ -13,7 +13,6 @@ include("config.php");
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-
 <!-- Meta Tags -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="description" content="Real Estate PHP">
