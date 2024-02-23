59206348bbe5b1a8c6b72ef6a44b7c8044393c9a
Raval balkrishna <112169855+Raval197@users.noreply.github.com>
2024-02-24 01:06:26 +0530

    hy

--- include/header.php
+++ include/header.php
@@ -52,7 +52,7 @@
 												<li class="nav-item"> <a class="nav-link" href="profile.php">Profile</a> </li>
 												<li class="nav-item"> <a class="nav-link" href="changepass.php">Change Password</a> </li>	
 												<li class="nav-item"> <a class="nav-link" href="feedback.php">Feedback</a> </li>
-												<li class="nav-item"> <a class="nav-link" href="feature.php">Your Property</a> </li>
+												<!-- <li class="nav-item"> <a class="nav-link" href="feature.php">Your Property</a> </li> -->
 											</ul>
                                         </li>
 										<?php } else { ?>
