68cb0749f61ca1ff8ab1b5e7d7ef042ba196037b
dharmiksa <141383430+dharmiksa@users.noreply.github.com>
2024-03-07 18:29:24 +0530

    okk

--- include/header.php
+++ include/header.php
@@ -8,6 +8,33 @@
                         <li><a href="#"><i class="fas fa-envelope text-success mr-1"></i>essen@realestatest.com</a></li>
                     </ul>
                 </div>
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+            </div>
+            <div class="main-nav secondary-nav hover-success-nav py-2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+<<<<<<< Updated upstream
+                            <nav class="navbar navbar-expand-lg navbar-light p-0"> <a class="navbar-brand position-relative" href="index.php"><img class="nav-logo" src="./images/logo/restatelg1.png" style="height: 60px;width: 60px;" alt=""></a>
+=======
+                            <nav class="navbar navbar-expand-lg navbar-light p-0"> <a class="navbar-brand position-relative" href="index.php"><img class="nav-logo" src="images/logo/restatelg1.png" alt=""></a>
+>>>>>>> Stashed changes
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul class="navbar-nav mr-auto">
+                                        <li class="nav-item dropdown"> <a class="nav-link" href="index.php" role="button" aria-haspopup="true" aria-expanded="false">Home</a></li>
+										
+										<li class="nav-item"> <a class="nav-link" href="about.php">About</a> </li>
+										
+                                        <li class="nav-item"> <a class="nav-link" href="contact.php">Contact</a> </li>										
+										
+                                        <li class="nav-item"> <a class="nav-link" href="property.php">Properties</a> </li>
+                                        
+                                      
+=======
+>>>>>>> Stashed changes
                 <div class="col-md-4">
                     <div class="top-contact float-right">
                         <ul class="list-text-white d-table">
@@ -16,6 +43,10 @@
                                     <a href="logout.php">Logout</a>&nbsp;&nbsp;<?php }
                                      else { ?>
                                     <a href="login.php">Login</a>&nbsp;&nbsp;
+<<<<<<< Updated upstream
+=======
+>>>>>>> 9cd5781a3702e48b51356cb69570c121d8ce9305
+>>>>>>> Stashed changes
 
                                     |
                             </li>
